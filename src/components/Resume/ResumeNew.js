6fbb19f441d2267217de7d71af16173c2e707540
rishik <rishikjanaswamy999@gmail.com>
2022-07-28 15:05:37 -0400

    Udpaing resume section

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -2,14 +2,14 @@ import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
-import pdf from "../../Assets/../Assets/Soumyajit_Behera-BIT_MESRA.pdf";
+import pdf from "../../Assets/../Assets/Rishik_Janaswamy_Resume(1).pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
 const resumeLink =
-  "https://raw.githubusercontent.com/soumyajit4419/portfolio/master/src/Assets/Soumyajit_Behera-BIT_MESRA.pdf";
+  "/Users/rishikdev/Desktop/SummerProjects/Web Dev react/Web-Dev-React/src/Assets/Rishik_Janaswamy_Resume(1).pdf";
 
 function ResumeNew() {
   const [width, setWidth] = useState(1200);
