3d5001cd94535509610f8bd4816be19bfaa433f2
rishik <rishikjanaswamy999@gmail.com>
2022-09-08 17:49:29 -0400

    Updating Projects

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -4,11 +4,12 @@ import ProjectCard from "./ProjectCards";
 import Particle from "../Particle";
 import chatify from "../../Assets/Projects/newNLP.png";
 import ProjectTechstack from "./ProjectTechstack";
-import FE from "../../Assets/Projects/FE.jpeg"
-import mario from "../../Assets/Projects/mario.png"
-import brain from "../../Assets/Projects/brain.png"
-import reactIcon from "../../Assets/Projects/react.png"
-import QandA from "../../Assets/Projects/Q&A.png"
+import FE from "../../Assets/Projects/FE.jpeg";
+import EA from "../../Assets/Projects/ea.jpeg";
+import mario from "../../Assets/Projects/mario.png";
+import brain from "../../Assets/Projects/brain.png";
+import reactIcon from "../../Assets/Projects/react.png";
+import QandA from "../../Assets/Projects/Q&A.png";
 
 function Projects() {
   return (
@@ -22,6 +23,16 @@ function Projects() {
           Here are a few projects I've worked on recently.
         </p>
         <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
+
+        <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={EA}
+              isBlog={false}
+              title="Exchange Arbitrage"
+              description="Built a corss exchange arbitrage HFT (model proof of concept) capitalizing on exchange artbirage on crypto markets." 
+              ghLink="https://github.com/Rishik-J/QuantFinance/tree/main/Cross%20Exchange%20Arbitrage"
+            />
+          </Col>
           <Col md={4} className="project-card">
             <ProjectCard
               imgPath={chatify}
@@ -29,7 +40,7 @@ function Projects() {
               title="Automated News Crawler with Sentiment Analysis"
               description="Created an Automated news crawler to find, summerize, and understand sentiment arround relevent news to influence investment decision. 
               Leverged Python and NLP"
-              ghLink="https://github.com/Rishik-J/Python-ML-Projects/blob/main/AutomatedStockNewsSentimentAnalysis.ipynb"
+              ghLink="https://github.com/Rishik-J/NLP-RLE/blob/main/Sentiment%20Analysis/AutomatedStockNewsSentimentAnalysis.ipynb"
             />
           </Col>
 
@@ -37,10 +48,9 @@ function Projects() {
             <ProjectCard
               imgPath={FE}
               isBlog={false}
-              title="Financial Engineering"
-              description="Using staistics to develop algorithms to model financal data. Created a 
-              Binomial assest pricing model for European Options and Implimented the Black-Sholes model in python. Continuing to work and improve my financial engineering skils through projects like these ."
-              ghLink="https://github.com/Rishik-J/Python-ML-Projects/tree/main/Quant"
+              title="Options Pricing"
+              description="Using staistics to develop algorithms to price Optionns Contrcts such as a Binomial assest pricing model for European Options and Implimented the Black-Sholes model in python." 
+              ghLink="https://github.com/Rishik-J/QuantFinance/tree/main/Options%20Pricing"
             />
           </Col>
 
@@ -81,6 +91,15 @@ function Projects() {
               title="Sentiment Analysis using Deep Learning"
               description="Trained a range of NLP models leveraging neural networks to detect sentiment arround news and tweets. Leveraged Python and TensorFlow"
               ghLink="https://github.com/Rishik-J/Python-ML-Projects/tree/main/Sentiment%20Analysis"
+            />
+          </Col>
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={brain}
+              isBlog={false}
+              title="Deep Learning"
+              description="Projects using Neural Networks on large alternative datasets"
+              ghLink="https://github.com/Rishik-J/Deep-Learning-Python-"
             />
           </Col>
         </Row>
